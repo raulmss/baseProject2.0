8835a0583e704da2412cf7861aa299e014c16d14
Raul Souto <raul.soutos96@gmail.com>
2024-04-17 09:56:27 -0500

    project initializing

---
+++ src/test/java/com/baseProject20/general/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseProject20.general;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
